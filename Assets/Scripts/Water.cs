d06327b6496ea5945e237b370d4892979cd723e6
fengzhouxuan <fengzhouxuan@qq.com>
2023-02-18 22:56:14 +0800

    水面shader波浪数值和逻辑波浪联动

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -23,24 +23,18 @@ namespace ML
 
         public float GetWaterLine(Vector3 worldPosition)
         {
-            // var local = transform.InverseTransformPoint(worldPosition);
-            var local = worldPosition;
-            var y1 = Mathf.Sin((local.x+Time.timeSinceLevelLoad*_speed)*_wave)*_height;
-            var y2 = Mathf.Cos((local.z+Time.timeSinceLevelLoad*_speed)*_wave)*_height;
-            return (y1+y2)/2+_waterLine;
+            float x = worldPosition.x;
+            float z = worldPosition.z;
+            var y1 = Mathf.Sin((x*_wave+Time.timeSinceLevelLoad*_speed))*_height;
+            var y2 = Mathf.Sin((z*_wave+Time.timeSinceLevelLoad*_speed))*_height;
+            return (y1+y2)+_waterLine;
         }
         private void Awake()
         {
             _transform = transform;
             _waterLine = _transform.position.y;
         }
-
-        private void FixedUpdate()
-        {
-            var sinT = Time.timeSinceLevelLoad;
-            var sin = Mathf.Sin(sinT * _speed);
-        }
-
+        
         private void OnDrawGizmos()
         {
             int count = 50;
@@ -50,9 +44,9 @@ namespace ML
                 {
                     float x = -10 + i*20f/count;
                     float z = -10 + j*20f/count;
-                    var y1 = Mathf.Sin((x+Time.timeSinceLevelLoad*_speed)*_wave)*_height;
-                    var y2 = Mathf.Cos((z+Time.timeSinceLevelLoad*_speed)*_wave)*_height;
-                    Vector3 pos = new Vector3(x, (y1+y2)/2+_waterLine, z);
+                    var y1 = Mathf.Sin((x*_wave+Time.timeSinceLevelLoad*_speed))*_height;
+                    var y2 = Mathf.Sin((z*_wave+Time.timeSinceLevelLoad*_speed))*_height;
+                    Vector3 pos = new Vector3(x, (y1+y2)+_waterLine, z);
                     Gizmos.color = Color.green;
                     Gizmos.DrawSphere(pos,0.1f);
                 }
